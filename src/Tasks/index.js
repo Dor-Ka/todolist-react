f795a0b18aad4b9d637367f593ea408ec8aa5745
Dorota Karpińska <dorota.k159@wp.pl>
2023-12-30 15:01:58 +0100

    added key: task.id

--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -4,6 +4,7 @@ const Tasks = ({ tasks, hideDoneTasks }) => (
   <ul className="tasks">
     {tasks.map(task => (
       <li
+        key={task.id}
         className={`tasks__item${task.done && hideDoneTasks
           ? " tasks__item--hidden"
           : ""} `}

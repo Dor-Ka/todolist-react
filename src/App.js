60d4191dca3a6d4c5a9ad24d1bb41093f6331ca0
Dorota Karpińska <dorota.k159@wp.pl>
2024-01-03 18:52:56 +0100

    added interaction: toggle task done

--- src/App.js
+++ src/App.js
@@ -21,6 +21,15 @@ function App() {
     setTasks(tasks => tasks.filter(task => task.id !== id));
   };
 
+  const toggleTaskDone = (id) => {
+    setTasks(tasks => tasks.map(task => {
+      if (task.id == id) {
+        return { ...task, done: !task.done };
+      }
+      return task;
+    }))
+  }
+
   return (
     <Container>
       <Header title="Lista zadań" />
@@ -34,7 +43,10 @@ function App() {
         body={
           <Tasks tasks={tasks}
             hideDone={hideDone}
-            removeTask={removeTask} />}
+            removeTask={removeTask}
+            toggleTaskDone={toggleTaskDone}
+          />
+        }
         extraHeaderContent={
           <Buttons
             tasks={tasks}

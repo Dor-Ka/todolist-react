4544917211f1b0cc31beba2d93b16077a3ef66d5
Dorota Karpińska <dorota.k159@wp.pl>
2024-02-19 15:48:35 +0100

    code formated and removed comments

--- src/App.js
+++ src/App.js
@@ -7,60 +7,15 @@ import Header from "./Header";
 import Container from "./Container";
 import { useTasks } from "./useTasks";
 
-
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  // const [tasks, setTasks] = useState(
-  //   JSON.parse(localStorage.getItem("tasks")) || []
-  // );
-
-  // useEffect(() => {
-  //   localStorage.setItem("tasks", JSON.stringify(tasks));
-  // }, [tasks]);
 
   const toggleHideDone = () => {
     setHideDone((hideDone) => !hideDone);
   };
 
-  // const removeTask = (id) => {
-  //   setTasks((tasks) => tasks.filter((task) => task.id !== id));
-  // };
-
-  // const toggleTaskDone = (id) => {
-  //   setTasks((tasks) =>
-  //     tasks.map((task) =>
-  //       task.id === id ? { ...task, done: !task.done } : task
-  //     )
-  //   );
-  // };
-
-  // const setAllDone = () => {
-  //   setTasks((tasks) =>
-  //     tasks.map((task) => ({
-  //       ...task,
-  //       done: true,
-  //     }))
-  //   );
-  // };
-
-  // const addNewTask = (content) => {
-  //   setTasks((tasks) => [
-  //     ...tasks,
-  //     {
-  //       content,
-  //       done: false,
-  //       id: tasks.length ? tasks[tasks.length - 1].id + 1 : 1,
-  //     },
-  //   ]);
-  // };
-
-const {
-  tasks,
-  removeTask,
-  toggleTaskDone,
-  setAllDone,
-  addNewTask,
-} = useTasks();
+  const { tasks, removeTask, toggleTaskDone, setAllDone, addNewTask } =
+    useTasks();
 
   return (
     <Container>

6661b312a4baa62584350e0985ee80fd7158729a
Dorota Karpińska <dorota.k159@wp.pl>
2024-03-15 12:12:32 +0100

    deleted comments in form

--- src/features/tasks/Form/index.js
+++ src/features/tasks/Form/index.js
@@ -5,43 +5,40 @@ import { addTask } from "../taskSlice";
 import { nanoid } from "@reduxjs/toolkit";
 
 const Form = () => {
-    const [newTaskContent, setNewTaskContent] = useState("");
-    const inputRef = useRef(null);
+  const [newTaskContent, setNewTaskContent] = useState("");
+  const inputRef = useRef(null);
 
-    const dispatach = useDispatch();
+  const dispatach = useDispatch();
 
-    const onFormSubmit = (event) => {
-        event.preventDefault();
+  const onFormSubmit = (event) => {
+    event.preventDefault();
 
-        const trimedNewTaskContent = newTaskContent.trim();
+    const trimedNewTaskContent = newTaskContent.trim();
 
-        // if (trimedNewTaskContent !== "") {
-        //     addNewTask(trimedNewTaskContent);
-        // };
-        dispatach(addTask({
-            content: trimedNewTaskContent,
-            done: false,
-            id: nanoid(),
-        }));
-
-        setNewTaskContent("");
-        inputRef.current.focus();
-    };
-
-    return (
-        <TasksForm onSubmit={onFormSubmit}>
-            <Input
-                ref={inputRef}
-                value={newTaskContent}
-                placeholder="Co jest do zrobienia?"
-                onChange={({ target }) => setNewTaskContent(target.value)}
-                name="tasks"
-           />
-            <Button>
-                Dodaj zadanie
-            </Button>
-        </TasksForm>
+    dispatach(
+      addTask({
+        content: trimedNewTaskContent,
+        done: false,
+        id: nanoid(),
+      })
     );
-}
+
+    setNewTaskContent("");
+    inputRef.current.focus();
+  };
+
+  return (
+    <TasksForm onSubmit={onFormSubmit}>
+      <Input
+        ref={inputRef}
+        value={newTaskContent}
+        placeholder="Co jest do zrobienia?"
+        onChange={({ target }) => setNewTaskContent(target.value)}
+        name="tasks"
+      />
+      <Button>Dodaj zadanie</Button>
+    </TasksForm>
+  );
+};
 
 export default Form;

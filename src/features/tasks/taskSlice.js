ed17ab13c96cde86848ef2b2189b381b3595d235
Dorota Karpińska <dorota.k159@wp.pl>
2024-03-15 13:24:58 +0100

    changed action name in reducers in taskSlice

--- src/features/tasks/taskSlice.js
+++ src/features/tasks/taskSlice.js
@@ -7,14 +7,14 @@ const taskSlice = createSlice({
     hideDone: false,
   },
   reducers: {
-    addTask: ({ tasks }, { payload }) => {
-      tasks.push(payload);
+    addTask: ({ tasks }, { payload: task }) => {
+      tasks.push(task);
     },
     toggleHideDone: (state) => {
       state.hideDone = !state.hideDone;
     },
-    toggleTaskDone: ({ tasks }, { payload }) => {
-      const index = tasks.findIndex((task) => task.id === payload);
+    toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex((task) => task.id === taskId);
       tasks[index].done = !tasks[index].done;
     },
     setAllDone: ({ tasks }) => {
@@ -22,8 +22,8 @@ const taskSlice = createSlice({
         task.done = true;
       }
     },
-    removeTask: ({ tasks }, { payload }) => {
-      const index = tasks.findIndex((task) => task.id === payload);
+    removeTask: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex(({id}) => id === taskId);
       tasks.splice(index, 1);
     },
   },

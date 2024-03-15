f0e58cc9c558e9513e253daa568dd657a63324a5
Dorota Karpińska <dorota.k159@wp.pl>
2024-03-15 12:24:39 +0100

    section and container: change padding in mediaquery - mobile

--- src/common/Container/styled.js
+++ src/common/Container/styled.js
@@ -4,4 +4,7 @@ export const Container = styled.main`
     max-width: 800px;
     margin: 0 auto;
     padding: 20px;
+
+    @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+        padding: 10px;
 `;
--- src/common/Section/styled.js
+++ src/common/Section/styled.js
@@ -1,36 +1,36 @@
 import styled from "styled-components";
 
 export const Page = styled.section`
-    margin: 10px 0px;
-    background-color: ${({ theme }) => theme.color.white};
-    box-shadow: 0 0 3px ${({ theme }) => theme.color.silver};
+  margin: 10px 0px;
+  background-color: ${({ theme }) => theme.color.white};
+  box-shadow: 0 0 3px ${({ theme }) => theme.color.silver};
 `;
 
 export const Title = styled.h2`
-    margin: 0;
-    font-size: 20px;
+  margin: 0;
+  font-size: 20px;
 `;
 
 export const Header = styled.header`
-    margin: 0;
-    padding: 20px;
-    border-bottom: 1px solid ${({ theme }) => theme.color.silver};
-    font-size: 20px;
-    display: grid;
-    align-items: center;
-    grid-template-columns: auto auto;
-    grid-gap: 10px;
-    justify-content: space-between;
+  margin: 0;
+  padding: 20px;
+  border-bottom: 1px solid ${({ theme }) => theme.color.silver};
+  font-size: 20px;
+  display: grid;
+  align-items: center;
+  grid-template-columns: auto auto;
+  grid-gap: 10px;
+  justify-content: space-between;
 
-    @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
-        grid-template-columns: 1fr;
-}
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    grid-template-columns: 1fr;
+    padding: 10px;
+  }
 `;
 
 export const Body = styled.div`
-        padding: 20px;
+  padding: 20px;
+
+  @media(max-width: ${({ theme }) => theme.breakpoint.mobile}px) {
+    padding: 10px;
 `;
-
-
-
-

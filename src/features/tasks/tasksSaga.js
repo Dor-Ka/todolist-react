c8e692dd870eadfbefa8b76990aeb2d1460b0473
Dorota Karpińska <dorota.k159@wp.pl>
2024-04-06 13:56:11 +0200

    added delay in generator  fetchExampleTasksHandler in tasksSaga.js

--- src/features/tasks/tasksSaga.js
+++ src/features/tasks/tasksSaga.js
@@ -1,9 +1,10 @@
-import { takeEvery, call, put } from "redux-saga/effects";
+import { takeLatest, call, put, delay } from "redux-saga/effects";
 import { fetchExampleTasks, setTasks } from "./taskSlice";
 import { getExampleTasks } from "./getExampleTasks";
 
 function* fetchExampleTasksHandler() {
   try {
+    yield delay(1000);
     const exampleTasks = yield call(getExampleTasks);
     yield put(setTasks(exampleTasks));
   } catch (error) {
@@ -13,5 +14,5 @@ function* fetchExampleTasksHandler() {
 
 export function* watchFetchExampleTasks() {
   console.log("saga jest podłączona");
-  yield takeEvery(fetchExampleTasks.type, fetchExampleTasksHandler);
+  yield takeLatest(fetchExampleTasks.type, fetchExampleTasksHandler);
 }

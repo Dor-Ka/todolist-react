3ef5678d2e0ff31416162679987d2360f374ed79
Dorota Karpińska <dorota.k159@wp.pl>
2024-04-06 13:44:44 +0200

    created file: rootSaga

--- src/store.js
+++ src/store.js
@@ -1,18 +1,17 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore } from "@reduxjs/toolkit";
 import createSagaMiddleware from "redux-saga";
-import tasksReducer from './features/tasks/taskSlice';
-import { watchFetchExampleTasks } from './features/tasks/tasksSaga';
-
+import tasksReducer from "./features/tasks/taskSlice";
+import rootSaga from "./rootSaga";
 const sagaMiddleware = createSagaMiddleware();
 
 const store = configureStore({
-    reducer: {
-        tasks: tasksReducer,
-    },
-    middleware: (getDefaultMiddleware) => 
-        getDefaultMiddleware().concat(sagaMiddleware),
+  reducer: {
+    tasks: tasksReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(sagaMiddleware),
 });
 
-sagaMiddleware.run(watchFetchExampleTasks);
+sagaMiddleware.run(rootSaga);
 
 export default store;

1c6f986034706a0339587018236c941a836e3f59
Dorota Karpińska <dorota.k159@wp.pl>
2024-04-20 13:42:21 +0200

    updated applications after adding new selectors

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,10 +1,13 @@
 import { useSelector, useDispatch } from "react-redux";
 import { List, Item, Button, Content } from "./styled";
-import { selectTasks, toggleTaskDone, removeTask } from "../taskSlice";
+import { selectTasks, toggleTaskDone, removeTask, selectHideDone } from "../taskSlice";
 
 const TaskList = () => {
-  const { tasks, hideDone } = useSelector(selectTasks);
+  const tasks = useSelector(selectTasks);
+  const hideDone = useSelector(selectHideDone);
+
   const dispatch = useDispatch();
+  
   return (
     <List>
       {tasks.map((task) => (

2318becaf735fe822b985007ef8677edcfd7db99
Dorota Karpińska <dorota.k159@wp.pl>
2024-06-06 19:02:09 +0200

    Fixed display of task details

--- src/features/tasks/tasksSlice.js
+++ src/features/tasks/tasksSlice.js
@@ -56,15 +56,13 @@ export const {
   setTasks,
 } = tasksSlice.actions;
 export const selectTasksState = state => state.tasks;
-
 export const selectTasks = state => selectTasksState(state).tasks;
 export const selectHideDone = state => selectTasksState(state).hideDone;
 export const selectAreTasksEmpty = state => selectTasks(state).length === 0;
 export const selectIsEveryTaskDone = state => selectTasks(state).every(({ done }) => done);
 
-
 export const getTaskById = (state, taskId) =>
-  selectTasksState(state).find(({ id }) => id === taskId);
+  selectTasksState(state.tasks).find(({ id }) => id === taskId);
 
 export const selectTasksByQuery = (state, query) => {
   const tasks = selectTasksState(state);

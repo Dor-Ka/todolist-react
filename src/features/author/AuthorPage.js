174fcb984effe9b3ab1e2d0565d7873a6c4b3737
Dorota Karpińska <dorota.k159@wp.pl>
2024-07-26 21:26:03 +0200

    AuthorPage updated

--- src/features/author/AuthorPage.js
+++ src/features/author/AuthorPage.js
@@ -5,8 +5,16 @@ import StyledSection from "../../common/Section";
 function AuthorPage () {
     return (
   <Container>
-    <Header title="O autorze" />
-    <StyledSection title="Dorota Karpińska" body={<>coś tam ....</>} />
+    <Header title="Cześć 👋" />
+    <StyledSection 
+      title="...tu Dorota 😉" 
+      body={<>
+      <p>Witam Cię na mojej stronie, która powstała w wyniku nauki programowania front-endowego.</p>
+      <p>Aktualnie zgłębiam się w tajniki: HTML, CSS, JavaScript oraz React - stararam się opanować te technologie, by tworzyć coraz lepsze i bardziej zaawansowane projekty. </p>
+      <p> W mojej przygodzie z kodowaniem towarzyszy mi mój czworonożny przyjaciel Ares 🐶 </p>
+      <p> Zapraszam do zapoznania się z moimi projektami i śledzenia postępów w mojej programistycznej podróży!</p>
+      </>}
+     />
   </Container>
 )};
 
